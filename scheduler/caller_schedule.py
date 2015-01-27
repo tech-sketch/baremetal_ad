f33d53df095825c25dab563d2286b34bd86841d2
k-chino <chino.k.mail@gmail.com>
2015-01-27 14:52:59 +0900

    Add the real execution data allocation file

--- scheduler/caller_schedule.py
+++ scheduler/caller_schedule.py
@@ -6,14 +6,16 @@
 #
 #   caller_schedule <hostname>
 #
-
+import os.path
 from schedule.rule.rule import Rule
 
 class Schedule(object):
 
     def __init__(self, hostname):
-        self.candidate_proxy = ""
-        rule = Rule(host_name = hostname, api_url = "http://localhost/zabbix/api_jsonrpc.php", username = "Admin", password = "zabbix", filepath = "/usr/lib/zabbix/scheduler/schedule/conf/sample_rule.json")
-        rule.read_definition()
-        rule.check_current_settings()
-        self.candidate_proxy = rule.candidate_proxy
+        self.candidate_proxy = ''
+        path = "/usr/lib/zabbix/scheduler/schedule/conf/proxy_rule.json"
+        if os.path.exists(path):
+            rule = Rule(host_name = hostname, api_url = "http://localhost/zabbix/api_jsonrpc.php", username = "Admin", password = "zabbix", filepath = path)
+            rule.read_definition()
+            rule.check_current_settings()
+            self.candidate_proxy = rule.candidate_proxy
